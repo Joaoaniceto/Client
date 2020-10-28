3af083a248b6420e490f9fd50ac3fe00997e45a4
Joao Aniceto <joaojoao16@hotmail.com>
2020-10-28 11:46:20 +0000

    new server

--- server.js
+++ server.js
@@ -8,25 +8,48 @@ const helmet =  require('helmet');
 
 const app = express();
 
+const monk = require('monk')
+
+
+const db = monk('localhost:27017/dbshort');
+
+const collection = db.get('dbshort');
+
 app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./client'))
+app.use(express.static('./client'));
 
 
 app.get('/url/:id',(req,res)=>{
     // todo redirect
     })
 
-app.get('/:id',(req,res)=>{
-    // todo redirect
+app.get('/:id', async (req,res)=>{
+    const {id: id} = req.params;
+    try{
+    var a = await collection.findOne({name: id});
+    console.log(a); 
+    if(a){
+        return res.redirect(a.url);
+
+    }
+
+    } catch(err){
+console.log(err);
+    }
     })
 
 app.post('/url',(req,res)=>{
-   
+  collection.insert({
+      url:req.body.url,
+      name:req.body.name,
+  }).then(()=>{db.close()});
+  res.json({
+    url:req.body.url,
+    name:req.body.name,
+  })
     })
-
-
 
 app.listen(1337);
